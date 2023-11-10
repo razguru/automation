41203b4ef8e4ee97dc218c349b39621340b052fa
razguru <146895251+razguru@users.noreply.github.com>
2023-11-09 23:09:33 -0700

    Update gp2_gp3_migration_progress.sh

--- gp2_gp3_migration_progress.sh
+++ gp2_gp3_migration_progress.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # Author : @razguru
-# Version : 1.0
+# Version : 1.1 - Added cross account action through --profile assume role
 # Desc : Track modification status of volumes ..
 #
 vol_id_list="$1"
 region="$2"
+account="$4"
 dt=`date +%F-%T`
 log=""$region"_"$dt"_vol_modification_progress.log"
 
 #Ensure vol_id_list is provided
 if [ -z $vol_id_list ] || [ ! -s $vol_id_list ] ; then
-        echo "Please provide a file name listing target volume_ids .. example - $0 vol_id_list us-east-1"
+        echo "Please provide a file name listing target volume_ids followed by region_name .. example - $0 vol_id_list us-east-1"
 exit 1
 fi
 
@@ -22,7 +23,7 @@ fi
 
 
 for vol_id in `cat "$vol_id_list"`;do
-	echo `date +%F-%T` >> $log && /usr/bin/aws ec2 describe-volumes-modifications --region "${region}" --volume-ids "${vol_id}" --query "VolumesModifications[*].{ID:VolumeId,STATE:ModificationState,Progress:Progress}"; done| tee -a $log
+	echo `date +%F-%T` >> $log && /usr/bin/aws ec2 describe-volumes-modifications --region "${region}" --profile "${account}" --volume-ids "${vol_id}" --query "VolumesModifications[*].{ID:VolumeId,STATE:ModificationState,Progress:Progress}"; done| tee -a $log
 if [ $? -eq 0 ]; then
 	echo " Logged the output to $log"
 else
